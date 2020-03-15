16cd21bfd9ea7e3b4db48ee497997c8a2931069d
MarziaCarrera <akislykh@mail.ru>
2020-03-15 14:57:20 +0500

    Added user list page

--- src/main/java/com/urfusoftware/controllers/UserController.java
+++ src/main/java/com/urfusoftware/controllers/UserController.java
@@ -7,10 +7,12 @@ import com.urfusoftware.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class UserController {
@@ -31,5 +33,29 @@ public class UserController {
         model.addAttribute("user", user);
         model.addAttribute("role", roleRepository.findAll());
         return "user-edit";
+    }
+
+    @PostMapping("/user-edit/{user}")
+    public String userSave(
+            @RequestParam String username,
+            @RequestParam String surname,
+            @RequestParam String name,
+            @RequestParam String password,
+            @RequestParam("userId") User user,
+            @RequestParam String role, Model model
+    ) {
+        if (name.isEmpty() || surname.isEmpty() || username.isEmpty() || password.isEmpty()) {
+            model.addAttribute("message", "ОШИБКА! Поля не могут быть пустыми!");
+            return "redirect:/user-edit/{user}";
+        }
+        else {
+            user.setUsername(username);
+            user.setName(name);
+            user.setSurname(surname);
+            user.setPassword(password);
+            user.setRole(roleRepository.findAll().get(Integer.parseInt(role) - 1));
+            userRepository.save(user);
+        }
+        return "redirect:/user-list";
     }
 }
