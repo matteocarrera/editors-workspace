f393d5da1b9cb0f4e5deb6b551ecf8c7ed3cb11a
Vladimir <matteocarrera@mail.ru>
2020-03-15 12:35:07 +0500

    Fixed all bugs with User Edit page

--- src/main/java/com/urfusoftware/controllers/UserController.java
+++ src/main/java/com/urfusoftware/controllers/UserController.java
@@ -1,21 +1,22 @@
 package com.urfusoftware.controllers;
 
+import com.urfusoftware.domain.Role;
 import com.urfusoftware.domain.User;
 import com.urfusoftware.repositories.RoleRepository;
 import com.urfusoftware.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.BeanDefinitionDsl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.management.relation.Role;
+import java.util.List;
 
 @Controller
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
     private RoleRepository roleRepository;
 
     @GetMapping("/user-list")
@@ -25,10 +26,10 @@ public class UserController {
         return "user-list";
     }
 
-    @GetMapping("{/user}")
+    @GetMapping("/user-edit/{user}")
     public String userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
-        model.addAttribute("roles", roleRepository.findAll());
+        model.addAttribute("role", roleRepository.findAll());
         return "user-edit";
     }
 }
